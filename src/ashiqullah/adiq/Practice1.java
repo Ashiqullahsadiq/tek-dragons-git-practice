232de8b5dc84be209ceb22efa3db0fb95cc1795a
Ashiqullah Sadiq <ashiqullahsadiq44@gmail.com>
2023-06-01 18:09:56 -0700

    New class added from Ashiqullah

---
+++ src/ashiqullah/adiq/Practice1.java
@@ -0,0 +1,10 @@
+package ashiqullah.adiq;
+
+public class Practice1 {
+
+	public static void main(String[] args) {
+		System.out.println("Some Text from practice class");
+
+	}
+
+}
---
+++ src/ashiqullah/adiq/thisIsAshiqullahSadiq.java
@@ -0,0 +1,10 @@
+package ashiqullah.adiq;
+
+public class thisIsAshiqullahSadiq {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Everyone");
+
+	}
+
+}
